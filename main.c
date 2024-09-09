a9ba8e42afb7fd951d6d7caea8576cb7333d4a19
Volodymyr But <vbut.dev+git@gmail.com>
2024-09-09 11:40:25 +0200

    build(cmake): integrate a logging library

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
+#include <log.h>
+
 int main(int argc, char *argv[]) {
     (void)argc, (void)argv;
+    log_info("%s v%s [%s %s]",
+             REDBACK_PROJECT_NAME,
+             REDBACK_PROJECT_VERSION,
+             REDBACK_BUILD_TYPE,
+             REDBACK_BUILD_TIMESTAMP);
     return 0;
 }
