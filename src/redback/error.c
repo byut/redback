9d0db5c30f7cd1509fa5e77668e4f6732a1f96e3
Volodymyr But <vbut.dev+git@gmail.com>
2024-09-10 16:49:20 +0200

    feat(error): include system error codes into the enumeration

--- src/redback/error.c
+++ src/redback/error.c
@@ -1,9 +1,15 @@
+#include <asm-generic/errno.h>
 #include <redback/error.h>
+#include <string.h>
 
 /* # */
 
 const char *redback_strerror(enum redback_error error) {
-    switch (error) {
+    if (error <= EHWPOISON) {
+        return strerror(error);
+    }
+
+    switch ((unsigned short)error) {
     case REDBACK_SUCCESS:
         return "Success";
     case REDBACK_ERROR:
