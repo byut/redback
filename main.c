9a76c47f1747e9dc22dc5635b57d3195422bafa2
Volodymyr But <vbut.dev+git@gmail.com>
2024-10-01 15:25:44 +0200

    feat: setup I/O event base

--- main.c
+++ main.c
@@ -1,11 +1,52 @@
 #include <log.h>
 
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <event2/event.h>
+
+/* # */
+
+/// @brief Structure responsible for keeping track of pending and active I/O events.
+static struct event_base *evbase = NULL;
+
+/* # */
+
+/// @brief Start the application.
+static int run() {
+    int rv;
+    evbase = event_base_new();
+    if (NULL == evbase) {
+        rv = errno;
+        log_fatal("Couldn't initialize I/O : %s", strerror(rv));
+        return 1;
+    }
+
+    return 0;
+}
+
+/* # */
+
+static void cleanup();
+
 int main(int argc, char *argv[]) {
     (void)argc, (void)argv;
+    atexit(cleanup);
+
     log_info("%s v%s [%s %s]",
              REDBACK_PROJECT_NAME,
              REDBACK_PROJECT_VERSION,
              REDBACK_BUILD_TYPE,
              REDBACK_BUILD_TIMESTAMP);
-    return 0;
+
+    return run();
+}
+
+/* # */
+
+/// @brief Perform the necessary memory cleanup before the application terminates.
+static void cleanup() {
+    if (evbase)
+        event_base_free(evbase);
 }
