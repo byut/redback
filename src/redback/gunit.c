cf3414aaac4aa553a694495b79ce092a0a38834e
Volodymyr But <vbut.dev+git@gmail.com>
2024-10-10 16:39:29 +0200

    refactor(gunit): restructure source code

--- src/redback/gunit.c
+++ src/redback/gunit.c
@@ -2,7 +2,6 @@
 
 #include <assert.h>
 #include <fcntl.h>
-#include <limits.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -14,126 +13,143 @@
 
 #include <event2/event.h>
 
-/* # */
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+static int redback_gunit_refocus(struct redback_gunit *gunit);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+struct redback_text_view {
+    struct redback_gunit *gunit;
+    struct event *evoutput;
+    FILE *rpipe; /* read pipe */
+    FILE *wpipe;
+    WINDOW *win;
+    bool new_line;
+};
+
+struct redback_text_box {
+    struct redback_gunit *gunit;
+    struct event *evinput;
+    FILE *in;    /* input stream */
+    FILE *rpipe; /* read pipe */
+    FILE *wpipe; /* write pipe */
+    WINDOW *win;
+    char buf[1024];
+    unsigned long buflen;
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+static int redback_text_view_addch(struct redback_text_view *text_view, char ch) {
+    int rv;
+    if (text_view->new_line) {
+        text_view->new_line = false;
+        rv = waddch(text_view->win, '\n');
+        if (ERR == rv)
+            return rv;
+    }
+    if (ch == '\n') {
+        text_view->new_line = true;
+        return 0;
+    }
+    return waddch(text_view->win, ch);
+}
+
+static void redback_text_view_rpipe_read_callback(int readfd, short events, void *token) {
+    (void)readfd, (void)events, (void)token;
+    struct redback_text_view *text_view;
+    text_view = (struct redback_text_view *)token;
+    assert(readfd == fileno(text_view->rpipe));
+
+    [[maybe_unused]] int rv;
+    long lrv;
+    char readbuf[getmaxx(text_view->win) * getmaxy(text_view->win)];
+
+    while (true) {
+        lrv = read(fileno(text_view->rpipe), readbuf, sizeof(readbuf));
+        if (lrv <= 0)
+            break;
+        for (size_t i = 0, size = lrv; i < size; ++i) {
+            rv = redback_text_view_addch(text_view, readbuf[i]), assert(ERR != rv);
+        }
+    }
+
+    rv = wnoutrefresh(text_view->win), assert(ERR != rv);
+    rv = redback_gunit_refocus(text_view->gunit), assert(ERR != rv);
+    rv = curs_set(0), assert(ERR != rv);
+    rv = doupdate(), assert(ERR != rv);
+    rv = curs_set(1), assert(ERR != rv);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+static void redback_text_box_in_read_callback(int readfd, short events, void *token) {
+    (void)readfd, (void)events, (void)token;
+    struct redback_text_box *text_box;
+    text_box = (struct redback_text_box *)token;
+    assert(readfd == fileno(text_box->in));
+
+    [[maybe_unused]] int rv;
+    [[maybe_unused]] long lrv;
+    int key;
+
+    while (true) {
+        key = ERR;
+        key = wgetch(text_box->win);
+        if (ERR == key)
+            break;
+
+        if (key >= 32 && key <= 126 && text_box->buflen < sizeof(text_box->buf) - 1) {
+            text_box->buf[text_box->buflen++] = (char)key;
+            rv = waddch(text_box->win, key), assert(ERR != rv);
+            rv = wnoutrefresh(text_box->win), assert(ERR != rv);
+            rv = doupdate(), assert(ERR != rv);
+            continue;
+        }
+
+        switch (key) {
+        case '\n':
+            rv = werase(text_box->win), assert(ERR != rv);
+            rv = wnoutrefresh(text_box->win), assert(ERR != rv);
+            rv = doupdate(), assert(ERR != rv);
+            text_box->buf[text_box->buflen++] = '\n';
+            lrv = write(fileno(text_box->wpipe), text_box->buf, text_box->buflen);
+            assert(lrv > 0);
+            text_box->buflen = 0;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
 struct redback_gunit {
     struct event_base *evbase;
-
-    struct event *evin;
-    struct event *evsig;
-    struct event *evoutpipe;
+    struct event *evsigint;
 
     SCREEN *term;
     WINDOW *win;
-    WINDOW *focus;
+    FILE *out;
+    FILE *in;
 
-    /* # */
-
-    FILE *out_raw;
-    FILE *out_r;
-    FILE *out_w;
-    WINDOW *out_win;
-    bool out_nl;
-
-    /* # */
-
-    FILE *in_raw;
-    FILE *in_r;
-    FILE *in_w;
-    WINDOW *in_win;
-    char in_buf[1024];
-    int in_buflen;
-
-    /* # */
+    struct redback_text_view text_view;
+    struct redback_text_box text_box;
 
     redback_gunit_signal_callback on_signal;
 };
 
-/* # */
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
-static void redback_gunit_out_addch_h(struct redback_gunit *gunit, char ch) {
-    waddch(gunit->out_win, ch);
+static int redback_gunit_refocus(struct redback_gunit *gunit) {
+    return wnoutrefresh(gunit->text_box.win);
 }
 
-static void redback_gunit_out_addch(struct redback_gunit *gunit, char ch) {
-    if (gunit->out_nl) {
-        gunit->out_nl = false;
-        redback_gunit_out_addch_h(gunit, '\n');
-    }
-    if (ch == '\n') {
-        gunit->out_nl = true;
-        return;
-    }
-    redback_gunit_out_addch_h(gunit, ch);
-}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
-/* # */
-
-static void on_output(int readfd, short events, void *token) {
-    (void)readfd, (void)events, (void)token;
-    struct redback_gunit *gunit;
-    gunit = (struct redback_gunit *)token;
-
-    assert(readfd == fileno(gunit->out_r));
-
-    long lrv;
-    char readbuf[1024];
-
-    while (true) {
-        lrv = read(readfd, readbuf, sizeof(readbuf));
-        if (lrv <= 0)
-            break;
-        assert(lrv <= INT_MAX);
-        for (size_t i = 0, size = lrv; i < size; ++i) {
-            redback_gunit_out_addch(gunit, readbuf[i]);
-        }
-    }
-
-    /* # */
-
-    curs_set(0);
-
-    wrefresh(gunit->out_win);
-    wrefresh(gunit->in_win);
-
-    curs_set(1);
-}
-
-/* # */
-
-static void on_keyup(int readfd, short events, void *token) {
-    (void)readfd, (void)events, (void)token;
-    struct redback_gunit *gunit;
-    gunit = (struct redback_gunit *)token;
-
-    assert(readfd == fileno(gunit->in_raw));
-    assert(gunit->in_buflen >= 0);
-
-    int key = wgetch(gunit->in_win);
-
-    // check if |key| is an ASCII character.
-    if (key >= 32 && key <= 126 && (unsigned long)gunit->in_buflen < sizeof(gunit->in_buf)) {
-        gunit->in_buf[gunit->in_buflen++] = (char)key;
-        waddch(gunit->in_win, key);
-    }
-
-    switch (key) {
-    case '\n': {
-        gunit->in_buf[gunit->in_buflen++] = '\n';
-        write(fileno(gunit->in_w), gunit->in_buf, gunit->in_buflen);
-        wmove(gunit->in_win, 0, 0);
-        wclrtoeol(gunit->in_win);
-        gunit->in_buflen = 0;
-        break;
-    }
-    default:
-        break;
-    }
-
-    wrefresh(gunit->in_win);
-}
-
-static void on_redback_gunit_signal(int signal, short events, void *token) {
+static void redback_gunit_sigint_callback(int signal, short events, void *token) {
     (void)signal, (void)events, (void)token;
     struct redback_gunit *gunit;
     gunit = (struct redback_gunit *)token;
@@ -142,92 +158,36 @@ static void on_redback_gunit_signal(int signal, short events, void *token) {
     }
 }
 
-/* # */
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
 struct redback_gunit *redback_gunit_new(struct event_base *evbase) {
     struct redback_gunit *gunit;
     gunit = (struct redback_gunit *)malloc(sizeof(struct redback_gunit));
     if (NULL == gunit)
         return NULL;
-
+    memset(gunit, 0x00, sizeof(struct redback_gunit));
     gunit->evbase = evbase;
-
-    gunit->evin = NULL;
-    gunit->evsig = NULL;
-    gunit->evoutpipe = NULL;
-
-    gunit->term = NULL;
-    gunit->win = NULL;
-    gunit->focus = NULL;
-
-    gunit->out_raw = NULL;
-    gunit->out_r = NULL;
-    gunit->out_w = NULL;
-    gunit->out_win = NULL;
-    gunit->out_nl = false;
-
-    gunit->in_raw = NULL;
-    gunit->in_r = NULL;
-    gunit->in_w = NULL;
-    gunit->in_win = NULL;
-    memset(gunit->in_buf, 0, sizeof(gunit->in_buf));
-    gunit->in_buflen = 0;
-
-    gunit->on_signal = NULL;
-
     return gunit;
 }
 
-void redback_gunit_free(struct redback_gunit *gunit) {
-    if (gunit->evin) {
-        event_del(gunit->evin);
-        event_free(gunit->evin);
-    }
-    if (gunit->evsig) {
-        event_del(gunit->evsig);
-        event_free(gunit->evsig);
-    }
-    if (gunit->evoutpipe) {
-        event_del(gunit->evoutpipe);
-        event_free(gunit->evoutpipe);
-    }
-    if (gunit->out_r) {
-        fclose(gunit->out_r);
-    }
-    if (gunit->out_w) {
-        fclose(gunit->out_w);
-    }
-    if (gunit->in_r) {
-        fclose(gunit->in_r);
-    }
-    if (gunit->in_w) {
-        fclose(gunit->in_w);
-    }
-    free(gunit);
-}
-
-/* # */
-
 int redback_gunit_setup(struct redback_gunit *gunit, void *output, void *input, const char *term) {
-    assert(NULL == gunit->term);
-    assert(NULL == gunit->win);
-
     [[maybe_unused]] int rv;
+    int pipefds[2] = {-1, -1};
 
-    gunit->out_raw = output;
-    gunit->in_raw = input;
+    // Terminal Setup ///
 
-    gunit->term = newterm(term, gunit->out_raw, gunit->in_raw);
+    gunit->out = output;
+    gunit->in = input;
+
+    gunit->term = newterm(term, gunit->out, gunit->in);
     gunit->win = stdscr;
-    if (NULL == gunit->term)
+    if (NULL == gunit->term) {
         return -1;
+    }
 
-    curs_set(0);
-
+    rv = curs_set(0), assert(ERR != rv);
     rv = cbreak(), assert(ERR != rv);
     rv = noecho(), assert(ERR != rv);
-    rv = keypad(gunit->win, TRUE), assert(ERR != rv);
-    rv = nodelay(gunit->win, TRUE), assert(ERR != rv);
 
     if (has_colors()) {
         start_color();
@@ -244,54 +204,71 @@ int redback_gunit_setup(struct redback_gunit *gunit, void *output, void *input, 
         wbkgd(gunit->win, COLOR_PAIR(0));
     }
 
-    /* # */
+    // Signal Handlers //
 
-    int width = getmaxx(gunit->win);
-    int height = getmaxy(gunit->win);
-
-    gunit->out_win = newwin(height - 2, width, 0, 0);
-    if (NULL == gunit->out_win) {
-        return -1;
-    }
-    scrollok(gunit->out_win, TRUE);
-
-    gunit->in_win = newwin(1, width, height - 1, 0);
-    if (NULL == gunit->in_win) {
-        return -1;
-    }
-
-    // focus on |gunit->inbox|.
-    (void)wrefresh(gunit->win);
-    (void)wrefresh(gunit->out_win);
-    (void)wrefresh(gunit->in_win);
-    curs_set(1);
-    gunit->focus = gunit->in_win;
-
-    /* # */
-
-    gunit->evsig = event_new(
+    gunit->evsigint = event_new(
         gunit->evbase,
         SIGINT, EV_SIGNAL | EV_PERSIST,
-        on_redback_gunit_signal, gunit);
-    if (NULL == gunit->evsig) {
+        redback_gunit_sigint_callback, gunit);
+    if (NULL == gunit->evsigint) {
         return -1;
     }
-    event_add(gunit->evsig, 0);
+    event_add(gunit->evsigint, 0);
 
-    /* # */
+    // Text Box //
 
-    gunit->evin = event_new(
+    gunit->text_box.gunit = gunit;
+    gunit->text_box.in = gunit->in;
+    gunit->text_box.buflen = 0;
+    memset(gunit->text_box.buf, 0x00, sizeof(gunit->text_box.buf));
+
+    gunit->text_box.win = newwin(
+        1, getmaxx(gunit->win),
+        getmaxy(gunit->win) - 1, 0);
+    if (NULL == gunit->text_box.win) {
+        return -1;
+    }
+    rv = notimeout(gunit->text_box.win, TRUE), assert(ERR != rv);
+    rv = nodelay(gunit->text_box.win, TRUE), assert(ERR != rv);
+    rv = keypad(gunit->text_box.win, TRUE), assert(ERR != rv);
+    rv = wbkgd(gunit->text_box.win, COLOR_PAIR(0)), assert(ERR != rv);
+
+    rv = pipe(pipefds);
+    if (-1 == rv) {
+        return -1;
+    }
+
+    gunit->text_box.rpipe = fdopen(pipefds[0], "r");
+    if (NULL == gunit->text_box.rpipe) {
+        return -1;
+    }
+    gunit->text_box.wpipe = fdopen(pipefds[1], "w");
+    if (NULL == gunit->text_box.wpipe) {
+        return -1;
+    }
+
+    gunit->text_box.evinput = event_new(
         gunit->evbase,
-        fileno(gunit->in_raw), EV_READ | EV_PERSIST,
-        on_keyup, gunit);
-    if (NULL == gunit->evin) {
+        fileno(gunit->text_box.in),
+        EV_READ | EV_PERSIST,
+        redback_text_box_in_read_callback,
+        &gunit->text_box);
+    if (NULL == gunit->text_box.evinput) {
         return -1;
     }
-    event_add(gunit->evin, 0);
+    event_add(gunit->text_box.evinput, 0);
 
-    /* # */
+    // Text View //
 
-    int pipefds[2];
+    gunit->text_view.gunit = gunit;
+    gunit->text_view.new_line = false;
+
+    gunit->text_view.win = newwin(getmaxy(gunit->win) - 2, getmaxx(gunit->win), 0, 0);
+    if (NULL == gunit->text_view.win) {
+        return -1;
+    }
+    rv = scrollok(gunit->text_view.win, TRUE), assert(ERR != rv);
+    rv = wbkgd(gunit->text_box.win, COLOR_PAIR(0)), assert(ERR != rv);
 
     rv = pipe(pipefds);
     if (-1 == rv) {
@@ -303,41 +280,32 @@ int redback_gunit_setup(struct redback_gunit *gunit, void *output, void *input, 
         return -1;
     }
 
-    gunit->out_r = fdopen(pipefds[0], "r");
-    if (NULL == gunit->out_r) {
+    gunit->text_view.rpipe = fdopen(pipefds[0], "r");
+    if (NULL == gunit->text_view.rpipe) {
         return -1;
     }
-    gunit->out_w = fdopen(pipefds[1], "w");
-    if (NULL == gunit->out_w) {
+    gunit->text_view.wpipe = fdopen(pipefds[1], "w");
+    if (NULL == gunit->text_view.wpipe) {
         return -1;
     }
 
-    gunit->evoutpipe = event_new(
+    gunit->text_view.evoutput = event_new(
         gunit->evbase,
-        fileno(gunit->out_r), EV_READ | EV_PERSIST,
-        on_output, gunit);
-    if (NULL == gunit->evoutpipe) {
+        fileno(gunit->text_view.rpipe), EV_READ | EV_PERSIST,
+        redback_text_view_rpipe_read_callback, &gunit->text_view);
+    if (NULL == gunit->text_view.evoutput) {
         return -1;
     }
-    event_add(gunit->evoutpipe, 0);
+    event_add(gunit->text_view.evoutput, 0);
 
-    /* # */
+    // ... //
 
-    rv = pipe(pipefds);
-    if (-1 == rv) {
-        return -1;
-    }
+    rv = wnoutrefresh(gunit->win), assert(ERR != rv);
+    rv = wnoutrefresh(gunit->text_view.win), assert(ERR != rv);
+    rv = wnoutrefresh(gunit->text_box.win), assert(ERR != rv);
 
-    gunit->in_r = fdopen(pipefds[0], "r");
-    if (NULL == gunit->in_r) {
-        return -1;
-    }
-    gunit->in_w = fdopen(pipefds[1], "w");
-    if (NULL == gunit->in_w) {
-        return -1;
-    }
-
-    /* # */
+    rv = curs_set(1), assert(ERR != rv);
+    rv = doupdate(), assert(ERR != rv);
 
     return 0;
 }
@@ -345,16 +313,49 @@ int redback_gunit_setup(struct redback_gunit *gunit, void *output, void *input, 
 int redback_gunit_restore(struct redback_gunit *gunit) {
     [[maybe_unused]] int rv;
 
-    if (gunit->out_win) {
-        delwin(gunit->out_win);
+    // Text View //
+
+    if (gunit->text_view.evoutput) {
+        event_del(gunit->text_view.evoutput);
+        event_free(gunit->text_view.evoutput);
+    }
+    if (gunit->text_view.rpipe) {
+        fclose(gunit->text_view.rpipe);
+    }
+    if (gunit->text_view.wpipe) {
+        fclose(gunit->text_view.wpipe);
+    }
+    if (gunit->text_view.win) {
+        delwin(gunit->text_view.win);
     }
 
-    if (gunit->in_win) {
-        delwin(gunit->in_win);
+    // Text Box //
+
+    if (gunit->text_box.evinput) {
+        event_del(gunit->text_box.evinput);
+        event_free(gunit->text_box.evinput);
     }
+    if (gunit->text_box.rpipe) {
+        fclose(gunit->text_box.rpipe);
+    }
+    if (gunit->text_box.wpipe) {
+        fclose(gunit->text_box.wpipe);
+    }
+    if (gunit->text_box.win) {
+        delwin(gunit->text_box.win);
+    }
+
+    // Signal Handlers //
+
+    if (gunit->evsigint) {
+        event_del(gunit->evsigint);
+        event_free(gunit->evsigint);
+    }
+
+    // Terminal //
 
     if (NULL == gunit->win)
-        return 1;
+        return -1;
     rv = endwin(), assert(ERR != rv);
     gunit->win = NULL;
 
@@ -366,17 +367,21 @@ int redback_gunit_restore(struct redback_gunit *gunit) {
     return 0;
 }
 
-/* # */
+void redback_gunit_free(struct redback_gunit *gunit) {
+    free(gunit);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
 void *redback_gunit_get_input_stream(const struct redback_gunit *gunit) {
-    return gunit->in_r;
+    return gunit->text_box.rpipe;
 }
 
 void *redback_gunit_get_output_stream(const struct redback_gunit *gunit) {
-    return gunit->out_w;
+    return gunit->text_view.wpipe;
 }
 
-/* # */
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
 void redback_gunit_set_signal_callback(struct redback_gunit *gunit, redback_gunit_signal_callback callback) {
     gunit->on_signal = callback;
